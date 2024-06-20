07eea59eefab170663ba4ec0cd3e70430a94a915
stephen-kiyingi <steverone1989@gmail.com>
2024-06-20 16:10:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var C = "Welcome to Canada";
+document.write(C); // string Assignment.
+
+document.write("Leonidas told Persians, \"This is Sparta!\" Persians responded with reteriation.");// Escape Character Assignment
+
+document.write("\"The Kingdom of Heaven belongs to the children,"
+    +"Come to me those who are weary.\""
+    +"-Jesus");
+
+    var SQUAD = Foward = "Jake", Left ="James", Right ="John", Center ="Jagen";
+    document.write(Foward); // Multiple Variable Assignment.
+    
+    var x = (5+5); 
+    document.write(x); // Expression Assignment.
+     
