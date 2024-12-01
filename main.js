34dd10f42b420ea1181db042ee33e83d4b46a19f
stephen-kiyingi <steverone1989@gmail.com>
2024-12-01 14:16:50 -0500

    Add files via upload
    project3_Math_operators assignments

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
-var C = "Welcome to Canada";
-document.write(C); // string Assignment.
+function add() { //defining and naming the addition function
+var addition = 3 + 3;
+document.getElementById( "Math" ).innerHTML = addition; }
 
-document.write("Leonidas told Persians, \"This is Sparta!\" Persians responded with reteriation.");// Escape Character Assignment
+function sub() { //defining and naming the subtraction function
+    var subtraction = 6 - 3;
+    document.getElementById( "Math" ).innerHTML = subtraction; }
 
-document.write("\"The Kingdom of Heaven belongs to the children,"
-    +"Come to me those who are weary.\""
-    +"-Jesus");
+    function mult() { //defining and naming multiplication function
+        var multiplication = 6 * 3;
+        document.getElementById( "Math" ).innerHTML = multiplication; }   
 
-    var SQUAD = Foward = "Jake", Left ="James", Right ="John", Center ="Jagen";
-    document.write(Foward); // Multiple Variable Assignment.
-    
-    var x = (5+5); 
-    document.write(x); // Expression Assignment.
-     
+        function div() { //defining and naming division function
+            var division = 6 / 3;
+            document.getElementById( "Math" ).innerHTML = division; }
+
+            function random() { //defining and naming random
+            document.getElementById("ran").innerHTML = Math.random()*20; }
+
+            function nega() { //defining and naming negation_operator
+                var negation_operator = 6;
+                document.getElementById( "Math" ).innerHTML = -6; }
+
+                function mult_operations() { //defining and naming multiple operations
+                    var multiple_operations = (6 + 3) * 2 / 2 - 5;
+                    document.getElementById( "Math" ).innerHTML = multiple_operations; }
+
+                    function mod_operator() { //naming and defining modulus_operator
+                        var modulus_operator = 6 % 4;
+                        document.getElementById( "Math" ).innerHTML = modulus_operator; }
+
+                        function increment() {
+                        var x = 6
+                        x++; //add one to the value
+                        document.getElementById("incrementfig").innerHTML = x; }
+                        
+                        function decrement() {
+                        var x = 6.30
+                        x--; //subtract one to the value
+                        document.getElementById("decrementfig").innerHTML = x; }
+
+                
+        
