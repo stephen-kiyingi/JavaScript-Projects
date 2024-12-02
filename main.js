faf23b281a3af0c65e95cb33b10c76eba94ce808
stephen-kiyingi <steverone1989@gmail.com>
2024-12-02 11:52:03 -0500

    Add files via upload
    project4_Dictionaries assignment

--- main.js
+++ main.js
@@ -1,43 +1,10 @@
-function add() { //defining and naming the addition function
-var addition = 3 + 3;
-document.getElementById( "Math" ).innerHTML = addition; }
-
-function sub() { //defining and naming the subtraction function
-    var subtraction = 6 - 3;
-    document.getElementById( "Math" ).innerHTML = subtraction; }
-
-    function mult() { //defining and naming multiplication function
-        var multiplication = 6 * 3;
-        document.getElementById( "Math" ).innerHTML = multiplication; }   
-
-        function div() { //defining and naming division function
-            var division = 6 / 3;
-            document.getElementById( "Math" ).innerHTML = division; }
-
-            function random() { //defining and naming random
-            document.getElementById("ran").innerHTML = Math.random()*20; }
-
-            function nega() { //defining and naming negation_operator
-                var negation_operator = 6;
-                document.getElementById( "Math" ).innerHTML = -6; }
-
-                function mult_operations() { //defining and naming multiple operations
-                    var multiple_operations = (6 + 3) * 2 / 2 - 5;
-                    document.getElementById( "Math" ).innerHTML = multiple_operations; }
-
-                    function mod_operator() { //naming and defining modulus_operator
-                        var modulus_operator = 6 % 4;
-                        document.getElementById( "Math" ).innerHTML = modulus_operator; }
-
-                        function increment() {
-                        var x = 6
-                        x++; //add one to the value
-                        document.getElementById("incrementfig").innerHTML = x; }
-                        
-                        function decrement() {
-                        var x = 6.30
-                        x--; //subtract one to the value
-                        document.getElementById("decrementfig").innerHTML = x; }
-
-                
-        
+function my_dictionary () {
+    var Human = {
+        Name: "Jane Doe",
+        Age: 35,
+        City: "Vaughan",
+        Country: "Canada"
+    };
+    delete Human.Country;// this removes the KVP from the dictionary before it is displayed.
+    document.getElementById("Dictionary").innerHTML = Human.Country;
+}
